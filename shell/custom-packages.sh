9fdd12af87f00cfa85a4cfb0b471c6cd30ab7870
NK <17175018+nkscat@users.noreply.github.com>
2026-04-26 18:23:06 +0800

    随身wifi

--- shell/custom-packages.sh
+++ shell/custom-packages.sh
@@ -11,7 +11,7 @@
 # 高级卸载 by YT Vedio Talk
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-uninstall"
 # 极光主题 by github eamonxg
-#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-theme-aurora luci-app-aurora-config luci-i18n-aurora-config-zh-cn"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-theme-aurora luci-app-aurora-config luci-i18n-aurora-config-zh-cn"
 # 去广告adghome
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-adguardhome"
 # 代理相关
@@ -26,8 +26,8 @@
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-nekobox"
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES momo luci-app-momo luci-i18n-momo-zh-cn"
 # 同样是代理相关 但以下2个属于imm仓库内的软件 一般在build24.sh中已经集成 你也可以在此处调整它的去留 若去除组件则使用减号- 若添加则 不使用减号 或者 不处理
-#CUSTOM_PACKAGES="$CUSTOM_PACKAGES -luci-app-openclash"
-#CUSTOM_PACKAGES="$CUSTOM_PACKAGES -luci-i18n-homeproxy-zh-cn"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES -luci-app-openclash"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES -luci-i18n-homeproxy-zh-cn"
 # VPN
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-tailscale luci-i18n-tailscale-zh-cn"
 # 分区扩容 by sirpdboy 
@@ -40,9 +40,9 @@
 # 当luci-app-advancedplus插件开启时 需排除冲突项 luci-app-argon-config和luci-i18n-argon-config-zh-cn 减号代表排除
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-advancedplus luci-i18n-advancedplus-zh-cn -luci-app-argon-config -luci-i18n-argon-config-zh-cn"
 # MosDNS
-#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-mosdns luci-i18n-mosdns-zh-cn"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-mosdns luci-i18n-mosdns-zh-cn"
 # Turbo ACC 网络加速
-#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-turboacc"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-turboacc"
 # 应用过滤 openappfilter.com
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-appfilter luci-i18n-appfilter-zh-cn"
 # Lucky大吉 
@@ -203,3 +203,34 @@
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-xinetd-zh-cn"
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-xlnetacc-zh-cn"
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-zerotier-zh-cn"
+#随身wifi
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-usb2"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-usb3"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-mii"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-usb-net"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-usb-net-aqc111"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-usb-net-asix"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-usb-net-asix-ax88179"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-usb-net-cdc-eem"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-usb-net-cdc-ether"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-usb-net-cdc-mbim"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-usb-net-cdc-ncm"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-usb-net-cdc-subset"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-usb-net-dm9601-ether"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-usb-net-hso"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-usb-net-huawei-cdc-ncm"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-usb-net-ipheth"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-usb-net-kalmia"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-usb-net-kaweth"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-usb-net-lan78xx"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-usb-net-mcs7830"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-usb-net-pegasus"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-usb-net-pl"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-usb-net-qmi-wwan"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-usb-net-rndis"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-usb-net-rtl8150"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-usb-net-rtl8152"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-usb-net-sierrawireless"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-usb-net-smsc75xx"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-usb-net-smsc95xx"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-usb-net-sr9700"
